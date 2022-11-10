c6b614f5ddf6a12a9cb93553156448b502ee72cb
Josh Brunner <brunnerj@mit.edu>
2022-11-10 14:00:32 -0500

    skeleton for simple conceptnet bot

---
+++ conceptnet.py
@@ -0,0 +1,12 @@
+class ConceptNetGraph:
+    def __init__():
+        self.edges = {} #dictionary of nodes -> list of edges
+        pass
+    def parse_graph(self):
+        #read the file with all the edges in concept net, and put them in the dictionary
+        pass
+    def get_distance_k_neighbors(self, word, k):
+        #return a dictionary where keys are distance k words, and values are lists that are the path to that word
+        pass
+    def get_two_word_clue(self, word1, word2):
+        pass
