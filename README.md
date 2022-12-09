ca52cd7092df3f5f1a6801924ec5628321f14120
Josh Brunner <brunnerj@mit.edu>
2022-12-09 01:06:06 -0500

    make it easier to clone

--- README.md
+++ README.md
@@ -1,6 +1,13 @@
 # codenames
 6.8610 Codenames final project
 
+Installation:
+
+1. Clone this repo
+2. `pip install reqirements.txt`
+3. download the necessary word embeddings:
+ 
+
 Step 1: borrow scaffolding code from an existing codenames paper divyakoyy/codenames \\
 
 Step 2: write a basic objective function based on 1 similarity scoring function that takes a set of your cards and a set of opponent cards and evaluates how strong a given clue word is \\
--- get-data.sh
+++ get-data.sh
@@ -1,7 +1,15 @@
-pip install gdown
-// Download the embeddings
-gdown https://drive.google.com/uc?id=1F70CtbsoPPPDnV-ZAUq0i0Rrvtv6taoV
-// sudo apt-get install unzip
-unzip CodenamesData.zip
-rm CodenamesData.zip
-mv CodenamesData data
+#!/bin/bash
+
+#pip install gdown
+# Download the embeddings
+#gdown https://drive.google.com/uc?id=1F70CtbsoPPPDnV-ZAUq0i0Rrvtv6taoV
+#sudo apt-get install unzip
+#unzip CodenamesData.zip
+#rm CodenamesData.zip
+#mv CodenamesData data
+
+#download conceptnet
+wget --load-cookies /tmp/cookies.txt "https://docs.google.com/uc?export=download&confirm=$(wget --quiet --save-cookies /tmp/cookies.txt --keep-session-cookies --no-check-certificate 'https://docs.google.com/uc?export=download&id=1sE6NFOZy_q9dH0obGW29BEzNGrcUYcd3' -O- | sed -rn 's/.*confirm=([0-9A-Za-z_]+).*/\1\n/p')&id=1sE6NFOZy_q9dH0obGW29BEzNGrcUYcd3" -O conceptnet-assertions-en && rm -rf /tmp/cookies.txt
+
+#download numberbatch
+wget --no-check-certificate 'https://drive.google.com/uc?export=download&id=17Bs7zfjOxGQfjhHtBvTmR88o6m8Ecr4d' -O 'numberbatch-small.txt'
