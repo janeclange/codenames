4127514984d6bdc8b9135f2cc2f2a14c34704174
Jane Charlotte Lange <jlange@zoinks.csail.mit.edu>
2022-12-14 13:55:35 -0500

    input loops

--- hello.py
+++ hello.py
@@ -2,9 +2,12 @@
 
 import cgi 
 import cgitb 
+f = open("index.shtml", "r")
+page = f.read()
 
 form = cgi.FieldStorage()
 v = form.getvalue("guess")
 print("Content-type: text/html\n\n")
+print(page)
 print("success\n")
 print(v)
